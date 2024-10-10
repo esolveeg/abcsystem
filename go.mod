8cc5b7a1fbf8a298f25759d5175e2c8d60c345be
darwishdev <a.darwish.dev@gmail.com>
2024-10-10 12:52:05 +0300

    add zero log and db , grpc loggers

--- go.mod
+++ go.mod
@@ -8,8 +8,9 @@ require (
 	connectrpc.com/grpcreflect v1.2.0
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/rs/cors v1.11.1
+	github.com/rs/zerolog v1.33.0
 	github.com/spf13/viper v1.19.0
-	golang.org/x/net v0.23.0
+	golang.org/x/net v0.30.0
 	google.golang.org/protobuf v1.35.1
 )
 
@@ -20,6 +21,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
@@ -31,11 +34,11 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -15,8 +15,12 @@ import (
 	"github.com/darwishdev/devkit-api/gen/proto/devkit/v1/devkitv1connect"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/cors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+	"os"
+	"time"
 )
 
 func GrpcLogger() connect.UnaryInterceptorFunc {
@@ -25,7 +29,21 @@ func GrpcLogger() connect.UnaryInterceptorFunc {
 			ctx context.Context,
 			req connect.AnyRequest,
 		) (connect.AnyResponse, error) {
+			startTime := time.Now()
 			result, err := next(ctx, req)
+			duration := time.Since(startTime)
+			zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+			log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+			logger := log.Info()
+			if err != nil {
+				logger = log.Error().Err(err)
+			}
+			logger.
+				Str("Procedure", req.Spec().Procedure).
+				Interface("request", req.Any()).
+				Interface("response", result).
+				Dur("duration", duration).
+				Msg("received a gRPC request")
 			return result, err
 		})
 	}
@@ -46,34 +64,36 @@ func (api *Api) HelloWorld(ctx context.Context, req *connect.Request[apiv1.Hello
 func main() {
 	// first we need to simply create our grpc server
 	// in order to do that we will use buf build , buf connect
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	ctx := context.Background()
 
 	state, err := config.LoadState("./config")
 	if err != nil {
-		panic("can't load the config")
+		log.Fatal().Err(err).Msg("cannot load the state config")
 	}
+	dbTracer := db.NewDbTracer()
 	config, err := config.LoadConfig("./config", state.State)
 	dbConfig, err := pgxpool.ParseConfig(config.DBSource)
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("cannot load the config")
 	}
+	dbConfig.ConnConfig.Tracer = dbTracer
 	connPool, err := pgxpool.NewWithConfig(ctx, dbConfig)
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Interface("conntection", config.DBSource).Msg("cannot get the connection pool")
 	}
 	// Attempt to ping the database to ensure the connection is working.
 	if err := connPool.Ping(ctx); err != nil {
 		connPool.Close()
-		panic(err)
+		log.Fatal().Str("conn", config.DBSource).Err(err).Msg("database ping failed")
 	}
 	store := db.NewStore(connPool)
 	fmt.Println("connected to", config.DBSource, store)
-
 	mux := http.NewServeMux()
 	mux.Handle("/", http.RedirectHandler("https://darwishdev.com", http.StatusFound))
 	// here we can find examples of diffrent compression method 	https://connectrpc.com/docs/go/serialization-and-compression/#compression
 	compress1KB := connect.WithCompressMinBytes(1024)
-
 	interceptors := connect.WithInterceptors(GrpcLogger())
 	api := NewApi()
 	mux.Handle(devkitv1connect.NewDevkitServiceHandler(
@@ -102,7 +122,7 @@ func main() {
 
 	err = server.ListenAndServe()
 	if err != nil {
-		panic("can't start the server")
+		log.Fatal().Err(err).Msg("can't start the server")
 	}
 
 }
